2942b0ff4bc0c929ada96082eaedcf3d285e3d3a
unknown <atharv21240@iiitd.ac.in>
2024-08-08 16:40:11 +0530

    backend v1

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,37 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 const app = express();
 
-app.post("/hooks/catch/:userId/:zapId",(req,res)=>{
+
+
+const client = new PrismaClient();
+app.use(express.json());
+// const app = express(;)
+app.post("/hooks/catch/:userId/:zapId",async (req,res)=>{
     const userId= req.params.userId;
     const zapId= req.params.zapId;
+    // body is to abstract everything into body
+    const body= req.body;
+
     // store in db a new trigger
+    await client.$transaction(async tx=>{
+        const run = await client.zapRun.create({
+            data:{
+                zapId:zapId,
+                metadata:body
+            }
+        });
+        await client.zapRunOutbox.create({
+            data:{
+                zapRunId: run.id
+            }
+        })
+    })
+    res.json({
+        message:"webhook recieved"
+    })
+    
     // push it onto a queue- kafka or redis.
     
 });
+app.listen(3000);
