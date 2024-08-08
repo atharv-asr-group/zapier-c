d01657e01728f4c4efa802827c416de115cd52d3
unknown <atharv21240@iiitd.ac.in>
2024-08-08 19:09:16 +0530

    backend-transaction typo v1

--- src/index.ts
+++ src/index.ts
@@ -15,13 +15,13 @@ app.post("/hooks/catch/:userId/:zapId",async (req,res)=>{
 
     // store in db a new trigger
     await client.$transaction(async tx=>{
-        const run = await client.zapRun.create({
+        const run = await tx.zapRun.create({
             data:{
                 zapId:zapId,
                 metadata:body
             }
         });
-        await client.zapRunOutbox.create({
+        await tx.zapRunOutbox.create({
             data:{
                 zapRunId: run.id
             }
