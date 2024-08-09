25ea362f9a79e044497380c56d237e05e62c20a8
unknown <atharv21240@iiitd.ac.in>
2024-08-09 09:03:40 +0530

    added zod validation

--- primary-backend/src/router/user.ts
+++ primary-backend/src/router/user.ts
@@ -1,9 +1,18 @@
 import {Router} from "express";
 import { authMiddleware } from "./middleware";
+import { SignupSchema } from "../types";
 const router = Router();
 
 router.post("/signup",(req,res)=>{
-    console.log("signup handler");
+    // console.log("signup handler");
+    const body = req.body.username;
+    const parsedData=SignupSchema.safeParse(body);
+    if(!parsedData.success){
+        return res.status(411).json({
+            message:"Incorrect inputs"
+        })
+    }
+    // const userExists
 })
 router.post("signin",(req,res)=>{
     console.log("signin handler");
---
+++ primary-backend/src/types/index.ts
@@ -0,0 +1,9 @@
+import {z} from "zod";
+export const SignupSchema= z.object({
+    username:z.string().min(5),
+    password: z.string().min(6)
+})
+export const SigninSchema=z.object({
+    username:z.string(),
+    password:z.string()
+})
