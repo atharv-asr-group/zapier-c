c7488284fba424810731176076a28f05349e99ba
unknown <atharv21240@iiitd.ac.in>
2024-08-09 08:21:29 +0530

    template for basic be routes added

---
+++ primary-backend/src/router/zap.ts
@@ -0,0 +1,14 @@
+import {Router} from "express";
+import { authMiddleware } from "./middleware";
+const router = Router();
+
+router.post("/",authMiddleware,(req,res)=>{
+    console.log("create a zap");
+})
+router.get("/",authMiddleware,(req,res)=>{
+    console.log("signin handler");
+})
+router.get("/:zapId",authMiddleware,(req,res)=>{
+    console.log("signin handler");
+})
+export const zapRouter=router;
