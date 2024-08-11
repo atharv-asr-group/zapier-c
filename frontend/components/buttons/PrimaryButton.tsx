1d518888e939d365954c3f6fb1b15e4dc4a866d4
unknown <atharv21240@iiitd.ac.in>
2024-08-11 08:03:53 +0530

    producing frontend

---
+++ frontend/components/buttons/PrimaryButton.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react"
+
+export const PrimaryButton=({children, onClick, size="small"}:{
+    children:ReactNode,
+    onClick:()=>void,
+    size?:"big"|"small"
+})=>{
+    return <div className={`${size==="small"?"text-sm":"text-xl"}
+    ${size==="small"?"px-4 pt-2":"px-8 py-10"} bg-amber-700`}>
+        {children}
+    </div>
+}
